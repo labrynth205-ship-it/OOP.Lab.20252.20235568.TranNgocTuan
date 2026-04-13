d8de2c0c8d29f7c0154b212f16a8a46d442171d8
labrynth205-ship-it <labrynth205@gmail.com>
2026-04-13 16:10:15 +0700

    fix

---
+++ aimsproject/src/aimsproject/Aims.java
@@ -0,0 +1,19 @@
+package aimsproject;
+
+public class Aims {
+
+    public static void main(String[] args) {
+        Cart anOrder = new Cart();
+        DVD dvd1 = new DVD("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
+        DVD dvd2 = new DVD("Star Wars", "Science Fiction", "George Lucas", 124, 24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);
+        DVD dvd3 = new DVD("Aladdin", "Animation", 18.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+        System.out.println("Total Cost is: ");
+        System.out.println(anOrder.totalCost());
+        anOrder.removeDigitalVideoDisc(dvd2);
+        System.out.println("Total Cost after removing Star Wars is: " + anOrder.totalCost());
+    }
+
+}
